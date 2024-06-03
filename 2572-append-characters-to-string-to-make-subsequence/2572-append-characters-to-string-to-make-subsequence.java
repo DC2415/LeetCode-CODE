d2a28077a4512b236eb38fc297919185ac742e9d
DC2415 <123256305+DC2415@users.noreply.github.com>
2024-06-03 14:21:31 +0530

    Time: 7 ms (51.85%), Space: 45.3 MB (50.79%) - LeetHub

--- 2572-append-characters-to-string-to-make-subsequence/2572-append-characters-to-string-to-make-subsequence.java
+++ 2572-append-characters-to-string-to-make-subsequence/2572-append-characters-to-string-to-make-subsequence.java
@@ -3,12 +3,12 @@ class Solution {
         int i=0,j=0,c=0;
         while(i<s.length() && j<t.length()){
             if(s.charAt(i)==t.charAt(j)){
-          //      c++;
-                i++;
+              
+                
                 j++;
-            }else{
-                i++;
             }
+                i++;
+            
         }
         return t.length()-j;
     }
