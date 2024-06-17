5d059ad4c3c3adbfdb31b36463dbed9c3f3ed679
DC2415 <123256305+DC2415@users.noreply.github.com>
2024-06-17 09:54:32 +0530

    Time: 3 ms (97.79%), Space: 40.6 MB (14.02%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean judgeSquareSum(int c) {
+        int i=0;
+        int j=(int)(Math.sqrt(c));
+        while(i<=j){
+            long a= (long)(i*i)+(long)(j*j);
+            if(a==c){
+                return true;
+            }else if(a>c){
+            j--;}else{
+            i++;}
+        }
+        return false;
+    }
+}
