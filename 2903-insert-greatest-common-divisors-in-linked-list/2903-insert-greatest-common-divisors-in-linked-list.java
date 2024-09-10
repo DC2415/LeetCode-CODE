511dd060691cc28a04d9310c0915c9dd46736568
DHANANJAY CHACHERKAR <123256305+DC2415@users.noreply.github.com>
2024-09-10 10:27:11 +0530

    Time: 1 ms (100%), Space: 44.9 MB (61.64%) - LeetHub

--- 2903-insert-greatest-common-divisors-in-linked-list/2903-insert-greatest-common-divisors-in-linked-list.java
+++ 2903-insert-greatest-common-divisors-in-linked-list/2903-insert-greatest-common-divisors-in-linked-list.java
@@ -12,7 +12,7 @@ class Solution {
     public ListNode insertGreatestCommonDivisors(ListNode head) {
         ListNode slow = head;
         ListNode fast = head.next;
-        ListNode ans = new ListNode(0); // Dummy node
+        ListNode ans = new ListNode(0); 
         ListNode cur = ans;
 
         while (fast != null) {
@@ -23,28 +23,28 @@ class Solution {
           
             int gcdValue = gcd(slow.val, fast.val);
 
-            // Insert the GCD node between slow and fast
+           
             ListNode gcdNode = new ListNode(gcdValue);
             cur.next = gcdNode;
             cur = cur.next;
 
-            // Move the slow and fast pointers forward
+           
             slow = fast;
             fast = fast.next;
         }
 
-        // Attach the last node
+       
         cur.next = slow;
 
-        // Return the next node of the dummy node (which is the head of the new list)
+        
         return ans.next;
     }
 
-    // Function to calculate GCD
+   
     public int gcd(int a, int b) {
         if (b == 0) {
             return a;
         }
-        return gcd(b, a % b); // Return the recursive result
+        return gcd(b, a % b);
     }
 }
