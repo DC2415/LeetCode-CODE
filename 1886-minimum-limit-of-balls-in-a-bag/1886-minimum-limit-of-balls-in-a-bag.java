e91be5b4479294642e5dfb940da449ae2db7f6ab
DHANANJAY CHACHERKAR <123256305+DC2415@users.noreply.github.com>
2024-12-07 20:25:15 +0530

    Time: 25 ms (99.76%), Space: 60.5 MB (17.61%) - LeetHub

---
+++ 1886-minimum-limit-of-balls-in-a-bag/1886-minimum-limit-of-balls-in-a-bag.java
@@ -0,0 +1,43 @@
+class Solution {
+    private boolean helper(int[] nums,int maxOperations,int k)
+    {
+        int n=nums.length;
+        for(int i=0;i<n;i++)
+        {
+           if(nums[i]>k)
+           {
+            maxOperations-=(nums[i]-1)/k;
+           }
+           if(maxOperations<0)
+           {
+            return false;
+           } 
+        }
+        return true;
+    }
+    public int minimumSize(int[] nums, int maxOperations) {
+        
+        int max=nums[0];
+        int n=nums.length;
+
+        for(int i=0;i<n;i++)
+        {
+            if(max<nums[i])
+              max=nums[i];
+        }
+        int left=1,right=max;
+        while(left<=right)
+        {
+           int mid=left+(right-left)/2;
+           if(helper(nums,maxOperations,mid))
+           {
+            right=mid-1;
+           } 
+           else
+           {
+            left=mid+1;
+           }
+        }
+    return left;
+    }
+}
