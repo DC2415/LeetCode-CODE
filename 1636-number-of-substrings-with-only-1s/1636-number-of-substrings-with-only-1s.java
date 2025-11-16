daaf7fdbf3d96f48028d402ad6fa3412d156c89f
DHANANJAY CHACHERKAR <123256305+DC2415@users.noreply.github.com>
2025-11-16 14:26:17 +0530

    Time: 5 ms (57.07%), Space: 46.3 MB (9.16%) - LeetHub

--- 1636-number-of-substrings-with-only-1s/1636-number-of-substrings-with-only-1s.java
+++ 1636-number-of-substrings-with-only-1s/1636-number-of-substrings-with-only-1s.java
@@ -1,17 +1,32 @@
+// class Solution {
+//     public int numSub(String s) {
+//       return countSubstrings(s);
+//     }
+//     int countSubstrings(String s) {
+//     final int MOD = 1_000_000_007;
+//     long ans = 0;
+//     String[] segments = s.split("0"); 
+//     for (String seg : segments) {
+//         int len = seg.length();
+//         if (len > 0) {
+//             ans = (ans + (long) len * (len + 1) / 2) % MOD;
+//         }
+//     }
+//     return (int) ans;
+// }
+// }
 class Solution {
     public int numSub(String s) {
-      return countSubstrings(s);
-    }
-    int countSubstrings(String s) {
-    final int MOD = 1_000_000_007;
-    long ans = 0;
-    String[] segments = s.split("0"); 
-    for (String seg : segments) {
-        int len = seg.length();
-        if (len > 0) {
-            ans = (ans + (long) len * (len + 1) / 2) % MOD;
+        long cnt = 0, total = 0, mod = 1000000007;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '1') {
+                cnt++;
+            } else {
+                cnt = 0;
+            }
+            total = (total + cnt) % mod;
         }
+        return (int) total;
     }
-    return (int) ans;
 }
-}
+
