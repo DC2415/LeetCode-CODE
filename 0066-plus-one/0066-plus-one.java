26b3d8210239080e6a923e05a562fd183591499d
DHANANJAY CHACHERKAR <123256305+DC2415@users.noreply.github.com>
2026-01-01 11:50:06 +0530

    Time: 0 ms (100%), Space: 43.2 MB (89.12%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,43 +1,16 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        int []ans =new int[digits.length+1];
-        // if(digits[0]==9){
-        //    ans[0]=1;
-        //    for(int i=1 ;i<digits.length+1;i++){
-        //        ans[i]=0;
-        //    } 
-        //    return ans; 
-        // }
-        // if(digits[digits.length-1]<9){
-        //     int x= digits[digits.length-1]+1;
-        //     digits[digits.length-1]=x;
-        //     return digits;
-        // }
-        // for(int i=0 ;i<digits.length;i++){
-        //     if(digits[i]==9 && digits.length > 1){
-        //        int x= digits[i-1]+1;
-        //        digits[i-1]=x;
-        //        break;
-        //     }
-        // }
-        // for(int i=1 ;i<digits.length;i++){
-        //     if(digits[i]==9){
-        //        digits[i]=0;
-        //        //break;
-        //     }
-        // }
-        // return digits;
-        for(int i= digits.length-1;i>=0;i--){
-            if(digits[i]==9){
-                digits[i]=0;
-            }
-            else{
-                digits[i]+=1;
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
                 return digits;
             }
+            digits[i] = 0;
         }
-        ans[0]=1;
-        return ans;
-    }
 
+        // If all digits were 9
+        int[] result = new int[digits.length + 1];
+        result[0] = 1;
+        return result;
+    }
 }
